6341aea2799a4ec5d8226d6a18f7722ed3c2659f
Tomas Tokos <tokos@comto.sk>
2023-10-02 21:14:50 +0200

    Uprava sablony na zobrazenie prispevkov vybranych z databasy.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone   # pridane v kapitole 13
+from .models import Post    # pridane v kapitole 13
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})    # pridane
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  # pridane v kapitole 13
+    return render(request, 'blog/post_list.html', {'posts': posts})    # pridane a doplnene v kap. 13
+
